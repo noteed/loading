9858b391599135835737feddfc537dfd0cc6abe5
Vo Minh Thu <thu@hypered.io>
2020-10-18 21:08:16 +0200

    Use a render target texture.
    This fixes the problem with rendererLogicalSize, where some routine like
    drawLine or triangle are not properly scaled.

--- bin/loading.hs
+++ bin/loading.hs
@@ -15,10 +15,6 @@ main = do
   window <- createWindow "Loading..." defaultWindow
     { windowInitialSize = V2 1920 1200 }
   renderer <- createRenderer window (-1) defaultRenderer
-  -- Use big pixels, like an old video game. Instead of 320x240, I use
-  -- something similar but with a 1.6 aspect ratio (instead of 1.33).
-  -- Unfortunately, this doesn't affect sdl2-gfx's `triangle` routine.
-  rendererLogicalSize renderer $= (Just (V2 384 240))
 
   putStrLn "Press `q` to quit."
   loop renderer
@@ -37,7 +33,15 @@ loop :: Renderer -> IO ()
 loop renderer = do
   events <- pollEvents
 
-  -- Clear to red.
+  -- Create a render target with a low resolution and big pixels, and set it as
+  -- the current render target. Instead of 320x240, I use something similar but
+  -- with a 1.6 aspect ratio (instead of 1.33). Another way is to use
+  -- rendererLogicalSize but unfortunately, this doesn't affect sdl2-gfx's
+  -- `triangle` routine, nor even the normal `drawLine` routine.
+  target <- createTexture renderer RGBA8888 TextureAccessTarget (V2 384 240)
+  rendererRenderTarget renderer $= (Just target)
+
+  -- Clear to blue.
   rendererDrawColor renderer $= V4 0 0 204 255
   clear renderer
 
@@ -51,7 +55,19 @@ loop renderer = do
   -- How to draw a filled rectangle.
   fillRect renderer (Just (SDL.Rectangle (P (V2 50 10)) (V2 20 20)))
 
+  -- How to draw a line.
+  drawLine renderer (P (V2 10 40)) (P (V2 70 60))
+
+  -- Reset the render target to the default.
+  rendererRenderTarget renderer $= Nothing
+
+  -- Render the previsous render target to the default render target and
+  -- present it.
+  copy renderer target
+    (Just (SDL.Rectangle (P (V2 0 0)) (V2 384 240)))
+    (Just (SDL.Rectangle (P (V2 0 0)) (V2 1920 1200)))
   present renderer
+
   let qPressed = any isQPressed events
   unless qPressed (loop renderer)
 

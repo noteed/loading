4774456a5ecb104a9d3d7c48ca5f40bad5d9abf2
Vo Minh Thu <thu@hypered.io>
2022-08-21 14:41:13 +0200

    Compile with -O2.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@
 # Compile against sdl2-image for the IMG_SavePNG binding.
 # This is fragile as it relies on hard-coded path in the Nix store.
 # TODO I guess I have to use Cabal to properly set those ?
-ghc --make bin/loading.hs \
+ghc -O2 --make bin/loading.hs \
   -lSDL2_image \
   -I/nix/store/yzhqcvnhrbr6h1cdnw7pdb37jxg5cl34-SDL2_image-2.0.5/include/SDL2/ \
   -L/nix/store/yzhqcvnhrbr6h1cdnw7pdb37jxg5cl34-SDL2_image-2.0.5/lib
